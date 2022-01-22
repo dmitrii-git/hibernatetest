b8172992aa1f4018823a2b2664d4ee7922da5024
Dmitrii <mikheevda1@gmail.com>
2022-01-21 21:22:47 -0400

    Test1

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,27 +1,37 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 
 public class MainApp {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        EntityManagerFactory factory = new Configuration()
+        SessionFactory factory = new Configuration()
                 .configure("configs/hibernate.cfg.xml")
                 .buildSessionFactory();
-        EntityManager em = factory.createEntityManager();
+        Session session = null;
+        try {
+            session = factory.getCurrentSession();
+            session.beginTransaction();
 
-        Product product = context.getBean("product", Product.class);
+            Product product = context.getBean("product", Product.class);
+            product.setPrice(100);
+            product.setTitle("Pencil");
+            session.saveOrUpdate(product);
+            session.getTransaction().commit();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            factory.close();
+            if (session != null) {
+                session.close();
+            }
 
-        em.getTransaction().begin();
-        product.setPrice(100);
-        product.setTitle("Pencil");
-        em.persist(product);
-        em.getTransaction().commit();
+    }
 
-        em.close();
+
 
 
 
